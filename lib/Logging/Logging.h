a7839df0daaf4937585d1a2760b6742296314e5b
kreon3 <kreon.misc@gmail.com>
2018-07-31 10:48:52 -0700

    Rewriting logging to conditionally define the strings and logging functions only if they actually log.

--- lib/Logging/Logging.h
+++ lib/Logging/Logging.h
@@ -12,20 +12,54 @@
 
 #define LOG_LEVEL INFO
 
-#define LOG(LEVEL, MSG)                                                        \
-  if (LEVEL >= LOG_LEVEL)                                                      \
-    Serial.print(MSG);
+#if LOG_LEVEL <= DEBUG
+#define LOG_DEBUG_FUNC(FUNC, ...) FUNC(__VA_ARGS__);
+#else
+#define LOG_DEBUG_FUNC(FUNC, ...)
+#endif
 
-#define LOG_SPEC(LEVEL, MSG, SPEC)                                             \
-  if (LEVEL >= LOG_LEVEL)                                                      \
-    Serial.print(MSG, SPEC);
+#if LOG_LEVEL <= VERBOSE
+#define LOG_VERBOSE_FUNC(FUNC, ...) FUNC(__VA_ARGS__);
+#else
+#define LOG_VERBOSE_FUNC(FUNC, ...)
+#endif
 
-#define LOGLN(LEVEL, MSG)                                                      \
-  if (LEVEL >= LOG_LEVEL)                                                      \
-    Serial.println(MSG);
+#if LOG_LEVEL <= INFO
+#define LOG_INFO_FUNC(FUNC, ...) FUNC(__VA_ARGS__);
+#else
+#define LOG_INFO_FUNC(FUNC, ...)
+#endif
 
-#define LOGLN_SPEC(LEVEL, MSG, SPEC)                                           \
-  if (LEVEL >= LOG_LEVEL)                                                      \
-    Serial.println(MSG, SPEC);
+#if LOG_LEVEL <= WARNING
+#define LOG_WARNING_FUNC(FUNC, ...) FUNC(__VA_ARGS__);
+#else
+#define LOG_WARNING_FUNC(FUNC, ...)
+#endif
+
+#if LOG_LEVEL <= ERROR
+#define LOG_ERROR_FUNC(FUNC, ...) FUNC(__VA_ARGS__);
+#else
+#define LOG_ERROR_FUNC(FUNC, ...)
+#endif
+
+#if LOG_LEVEL <= NONE
+#define LOG_NONE_FUNC(FUNC, ...) FUNC(__VA_ARGS__);
+#else
+#define LOG_NONE_FUNC(FUNC, ...)
+#endif
+
+#define LOG(LEVEL, MSG) \
+  LOG_##LEVEL##_FUNC(Serial.print, MSG)
+
+#define LOG_SPEC(LEVEL, MSG, SPEC) \
+  LOG_##LEVEL##_FUNC(Serial.print, MSG, SPEC)
+
+#define LOGLN(LEVEL, MSG) \
+  LOG_##LEVEL##_FUNC(Serial.println, MSG)
+
+#define LOGLN_SPEC(LEVEL, MSG, SPEC) \
+  LOG_##LEVEL##_FUNC(Serial.println, MSG, SPEC)
+
+
 
 #endif // LIB_LOGGING_H_
